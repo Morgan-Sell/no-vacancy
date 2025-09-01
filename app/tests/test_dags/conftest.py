9f05404c07bd8fef2be8ab3066284a8eba2c4fb6
Morgan-Sell <morganpsell@gmail.com>
2025-08-31 19:41:09 -0700

    update ci.yaml and test_dags/confest.py and README

--- app/tests/test_dags/conftest.py
+++ app/tests/test_dags/conftest.py
@@ -1,6 +1,9 @@
 import pytest
+import importlib.util
 
-pytest.importorskip("airflow", reason="DAG tests require Apache Airflow")
+# If Airflow isn't installed (the unit job), tell pytest to ignore all tests in test_dags
+if importlib.util.find_spec("airflow") is None:
+    collect_ignore_glob = ["*.py"]  # don't attempt to import anything here
 
 
 @pytest.fixture(autouse=True)
