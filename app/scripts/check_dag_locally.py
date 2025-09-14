5fcd0309f27b2ac3c1751eff11706f1cc6a69331
Morgan-Sell <morganpsell@gmail.com>
2025-09-14 15:53:23 -0700

    changed DAG step in CI pipeline to install requirements/airflow.txt not dev.txt

--- app/scripts/check_dag_locally.py
+++ app/scripts/check_dag_locally.py
@@ -4,7 +4,6 @@ from pathlib import Path
 
 app_dir = Path(__file__).parent.parent
 sys.path.insert(0, str(app_dir))
-print("app_dir: ", app_dir)
 
 try:
     from dags.training_pipeline_dag import dag
